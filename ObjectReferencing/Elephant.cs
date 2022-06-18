a6749dd6d1ace483299666f2003af566a3c08e78
Brandon Sia <brandonsia@hotmail.com>
2022-06-18 14:26:24 +1000

    Add project files.

---
+++ ObjectReferencing/Elephant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectReferencing
+{
+    class Elephant
+    {
+        public int EarSize;
+        public string Name;
+
+        public void WhoAmI()
+        {
+            Console.WriteLine("My name is " + Name);
+            Console.WriteLine("My ears are " + EarSize + " inches tall.");
+        }
+    }
+}
